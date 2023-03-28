69e282f53cb066bf8f5ec41be2b14480fbe1ab76
Tony Bark <noreply@tonybark.com>
2023-03-27 21:14:23 -0400

    Curse of the Ferret Girl!

--- src/CaseyUniverse.DOC/Program.cs
+++ src/CaseyUniverse.DOC/Program.cs
@@ -4,17 +4,21 @@ using CaseyUniverse.DOC;
 using Ganss.Xss;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped<HtmlSanitizer, HtmlSanitizer>(x =>
 {
     var sanantize = new HtmlSanitizer();
-    sanantize.AllowedAttributes.Add("class");
+    // sanantize.AllowedAttributes.Add("class");
     return sanantize;
 });
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+});
 
 await builder.Build().RunAsync();
 
