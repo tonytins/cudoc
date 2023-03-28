4bfccaae09fa80b3ac3fadd2c9e99857a590a959
Tony Bark <noreply@tonybark.com>
2023-03-28 11:13:37 -0400

    Preliminary friendly URL support for galleries
    - Replaced JSON database file with a CSV
    - Images can now be viewed using a simple Id, similar to FurAffinity

--- src/CaseyUniverse.DOC/Models/Gallery.cs
+++ src/CaseyUniverse.DOC/Models/Gallery.cs
@@ -1,15 +1,8 @@
-﻿using System.Text.Json.Serialization;
+﻿using CsvHelper.Configuration.Attributes;
 
 namespace CaseyUniverse.DOC.Models;
 
 public record Gallery(
-    [property: JsonPropertyName("Title")] string Title,
-    [property: JsonPropertyName("Path")] string Path,
-    [property: JsonPropertyName("Source")] string Source,
-    [property: JsonPropertyName("Address")] string Address
+    [Name("Id")] int Id,
+    [Name("Filename")] string Filename
 );
-
-public record Databases(
-    [property: JsonPropertyName("gallery")] IReadOnlyList<Gallery> Gallery
-);
-
