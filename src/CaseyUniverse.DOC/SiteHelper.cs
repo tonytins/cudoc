2608d5ac632475c2866bd89eb9d3aa5462ed87e7
Tony Bark <noreply@tonybark.com>
2023-03-30 23:56:08 -0400

    Added a loading bar
    - Moved gifts, and added a few extra
    - Cleaned up site helper class
    - Renamed Submissions directory to Views
    - Added another former flash animation
    - Removed some excess images from the old DOC site

--- src/CaseyUniverse.DOC/SiteHelper.cs
+++ src/CaseyUniverse.DOC/SiteHelper.cs
@@ -1,33 +1,10 @@
-﻿using Markdig;
+using Markdig;
 using Microsoft.AspNetCore.Components;
 
 namespace CaseyUniverse.DOC;
 
 public static class SiteHelper
 {
-    /// <summary>
-    /// Resolves the image path based on the given image and path parameters.
-    /// </summary>
-    /// <param name="image">The name of the image.</param>
-    /// <param name="path">The name of the path.</param>
-    /// <returns>The resolved image path as a string.</returns>
-    public static string ResolveImagePath(string image, string path) => @$"../images/{path}/{image}";
-
-    /// <summary>
-    /// Returns the local CDN path based on the given csv and path parameters.
-    /// </summary>
-    /// <param name="csv">The name of the csv file.</param>
-    /// <param name="path">The name of the path.</param>
-    /// <returns>The local CDN path as a string.</returns>
-    public static string LocalCDN(string csv, string path) => $@"../data/{path}/{csv}.csv";
-
-    /// <summary>
-    /// Returns the local CDN path based on the given csv parameter.
-    /// </summary>
-    /// <param name="csv">The name of the csv file.</param>
-    /// <returns>The local CDN path as a string.</returns>
-    public static string LocalCDN(string csv) => $@"../data/{csv}.csv";
-
     /// <summary>
     /// Retrieves all the records from the provided CSV data and returns them as an enumerable collection of Gallery objects.
     /// </summary>
@@ -39,14 +16,6 @@ public static class SiteHelper
         using var file = new CsvReader(reader, CultureInfo.InvariantCulture);
         return file.GetRecords<IDatabase>().ToList();
     }
-
-    /// <summary>
-    /// Returns the permalink based on the given name and key parameters.
-    /// </summary>
-    /// <param name="name">The name of the permalink.</param>
-    /// <param name="key">The key for the permalink.</param>
-    /// <returns>The permalink as a string.</returns>
-    public static string Permalink(string name, string key) => @$"{name}/{key}";
 
     /// <summary>
     /// Converts the given Markdown content to HTML.
