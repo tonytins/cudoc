95ea87fa9985ae1d1032a4f0137c9865175e2bad
Tony Bark <noreply@tonybark.com>
2023-03-31 13:36:58 -0400

    DynTitle component
    - Page and heading titles as well as header data are now handled in a new DynTitle component
    - DynTitle has options for both page name, and optionally root/previous page
    - DynTitle will always show the site's name after the page  and optional root title from GlobalConsts.SITE_NAME
    - The "Woops! can't find the image" section is now located in ErrorDisplay component

--- src/CaseyUniverse.DOC/GlobalConsts.cs
+++ src/CaseyUniverse.DOC/GlobalConsts.cs
@@ -2,7 +2,7 @@
 
 public struct GlobalConsts
 {
-    public const string SITE_NAME = "Casey Universe DOC";
+    public const string SITE_NAME = "Casey Universe D.O.C.";
     public const string GALLERY_CSV = "data/gallery.csv";
     public const string COMIC_CSV = "data/comics.csv";
 }
