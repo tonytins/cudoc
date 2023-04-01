14f94223dd340637a95906de683121a163c178a8
Tony Bark <noreply@tonybark.com>
2023-04-01 15:36:22 -0400

    Automated galleries (#1)
    - Split the build and deploy actions
    - Both the gallery and partially the wallpaper page populate the page from the database.
    - Created a "submission" CSS class to hopefully solve a width issue related to Be My Buddy. To be fair, it was big even for DeviantART.
    - Wrote a simple ImagesFromPath() method for collecting listing images from said path. Pretty self-explanatory. XD
    - All artwork, except for stories and animations, are now featured on the homepage
    - Updated the copyright page
    - Brought back an old favicon
    - Shuffled gallery images
    - Simplified title algorithm
    - Merged model sheets into gallery

--- src/CaseyUniverse.DOC/SiteHelper.cs
+++ src/CaseyUniverse.DOC/SiteHelper.cs
@@ -1,15 +1,40 @@
 using Markdig;
 using Microsoft.AspNetCore.Components;
-
 namespace CaseyUniverse.DOC;
 
+/// <summary>
+/// A helper class for site-related functions.
+/// </summary>
 public static class SiteHelper
 {
     /// <summary>
-    /// Retrieves all the records from the provided CSV data and returns them as an enumerable collection of Gallery objects.
+    /// Gets a list of image files from a specified path.
     /// </summary>
-    /// <param name="csv">The CSV data stream.</param>
-    /// <returns>A list of Gallery objects.</returns>
+    /// <param name="csv">The CSV file stream containing gallery records.</param>
+    /// <param name="path">The path to filter the gallery records by.</param>
+    /// <returns>A list of image file names.</returns>
+    public static List<string> ImagesFromPath(Stream csv, string path)
+    {
+        var records = SiteHelper.DatabaseRecords<Gallery>(csv);
+        var files = new List<string>();
+
+        foreach (var record in records)
+        {
+            if (path == record.Path)
+            {
+                files.Add(record.Filename);
+            }
+        }
+
+        return files;
+    }
+
+    /// <summary>
+    /// Reads and deserializes records from a CSV file.
+    /// </summary>
+    /// <typeparam name="T">The type of database record.</typeparam>
+    /// <param name="csv">The CSV file stream to read from.</param>
+    /// <returns>A list of deserialized database records.</returns>
     public static List<IDatabase> DatabaseRecords<IDatabase>(Stream csv)
     {
         using var reader = new StreamReader(csv);
@@ -18,10 +43,10 @@ public static class SiteHelper
     }
 
     /// <summary>
-    /// Converts the given Markdown content to HTML.
+    /// Converts a Markdown-formatted string to an HTML-formatted string.
     /// </summary>
-    /// <param name="content">The Markdown content to convert.</param>
-    /// <returns>The HTML markup string.</returns>
+    /// <param name="content">The Markdown-formatted string to convert.</param>
+    /// <returns>An HTML-formatted string.</returns>
     public static MarkupString MarkdownToHtml(this string content)
     {
         if (!string.IsNullOrEmpty(content))
