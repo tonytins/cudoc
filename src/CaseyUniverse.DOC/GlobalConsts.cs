b78282098dc71881a4833c3e2dec0c716c16e9c2
Tony Bark <noreply@tonybark.com>
2023-04-06 02:46:58 -0400

    IPFS CDN

--- src/CaseyUniverse.DOC/GlobalConsts.cs
+++ src/CaseyUniverse.DOC/GlobalConsts.cs
@@ -6,5 +6,7 @@ public struct GlobalConsts
     public const string GALLERY_CSV = "data/gallery.csv";
     public const string COMIC_CSV = "data/comics.csv";
     public const string ANIMATIONS_CSV = "data/animations.csv";
+    public const string IPFS_ADDR = "QmY6YauXGkrJtGwFvE3RVukqwBiRaTqgDgY9ru8CY9bR9V";
+    public const string IPFS_CDN = @$"https://dweb.link/ipfs/{IPFS_ADDR}";
 }
 
