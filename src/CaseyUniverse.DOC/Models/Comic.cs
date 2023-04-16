06ba6a4da669496b9e87d7a4e4d40bb19d903513
Tony Bark <noreply@tonybark.com>
2023-04-16 04:59:06 -0400

    Comics page finally working
    - Updated dependencies

--- src/CaseyUniverse.DOC/Models/Comic.cs
+++ src/CaseyUniverse.DOC/Models/Comic.cs
@@ -5,8 +5,9 @@ namespace CaseyUniverse.DOC.Models;
 public record Comic
 {
     [Name("Id")] public int Id { get; set; } = 0;
-    [Name("Issue")] public string Issue { get; set; } = string.Empty;
-    [Name("Page")] public string Page { get; set; } = string.Empty;
+    [Name("Volume")] public string Volume { get; set; } = string.Empty;
+    [Name("Previous")] public int Previous { get; set; } = 0;
+    [Name("Next")] public int Next { get; set; } = 0;
     [Name("Filename")] public string Filename { get; set; } = string.Empty;
 }
 
